bc3a3b45ef962a8c459bd692fd38873f0f0e0133
KenjiTan <kenjitan2004@gmail.com>
2024-11-19 12:28:30 +0800

    Added database and seed data

---
+++ CarRentalManagement/Configurations/Entities/ColorSeed.cs
@@ -0,0 +1,37 @@
+﻿using CarRentalManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CarRentalManagement.Configurations.Entities
+{
+    public class ColorSeed : IEntityTypeConfiguration<Color>
+    {
+        public void Configure(EntityTypeBuilder<Color> builder)
+        {
+            builder.HasData(
+    new Color
+    {
+        Id = 1,
+        Name = "Black",
+        DateCreated = DateTime.Now,
+        DateUpdated = DateTime.Now,
+        CreatedBy = "System",
+        UpdatedBy = "System"
+    },
+    new Color
+    {
+        Id = 2,
+        Name = "Blue",
+        DateCreated = DateTime.Now,
+        DateUpdated = DateTime.Now,
+        CreatedBy = "System",
+        UpdatedBy = "System"
+    }
+);
+
+        }
+    }
+}
+
+
